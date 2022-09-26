c5ef00dda302a1c188160ae61ef04d0163ee846a
Cecilia Nievas <cnievas@gfz-potsdam.de>
2022-09-26 14:35:45 +0200

    Initialised repository structure

---
+++ real-time-loss-tools/realtimelosstools.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2022:
+#   Helmholtz-Zentrum Potsdam Deutsches GeoForschungsZentrum GFZ
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
+import logging
+import sys
+
+
+logger = logging.getLogger()
+
+
+def main():
+    """Run the programme."""
+
+    # Log the start of the run
+    logger.info("Real-Time Loss Tools has started")
+
+    # Leave the program
+    logger.info("Real-Time Loss Tools has finished")
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2022:
+#   Helmholtz-Zentrum Potsdam Deutsches GeoForschungsZentrum GFZ
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
+from setuptools import setup, find_packages
+
+tests_require = ["pytest"]
+
+setup(
+    name="real-time-loss-tools",
+    version="0.0.1",
+    description="",
+    keywords="earthquake damage, earthquake loss, rapid loss assessment, operational earthquake loss forecasting",
+    author="Helmholtz-Zentrum Potsdam Deutsches GeoForschungsZentrum GFZ",
+    license="AGPLv3+",
+    install_requires=[
+        "numpy",
+        "openquake.engine==3.13.0",
+    ],
+    extras_require={
+        "tests": tests_require,
+    },
+    packages=find_packages(),
+    entry_points={"console_scripts": ["rtlt = real-time-loss-tools.realtimelosstools:main"]},
+    python_requires=">=3.8",
+)
